20aba2558f3b5c96d0578ec0186e62e52b3dd6e9
Nicolas Ignacio Salinas <nicolasignacio.salinas@externos-cl.cencosud.com>
2024-01-29 21:33:09 -0300

    Password hash

--- src/authentication/auth.service.ts
+++ src/authentication/auth.service.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcryptjs from 'bcrypt';
 import { UserService } from '../modules/user/user.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -13,18 +14,26 @@ export class AuthService {
 
     async register(data: RegisterDto): Promise<any> {
         const exist = await this.userService.getByEmail(data.email);
+
         if (exist) {
-            throw new BadRequestException(`Ya existe usuario registrado con email '${data.email}'`)
+            throw new BadRequestException(`There is already an account with email '${data.email}'`)
         }
 
+        data.password = await bcryptjs.hash(data.password, 10);
         await this.userService.create(data);
     }
 
     async logIn(data: LoginDto): Promise<any> {
         const user = await this.userService.getByEmail(data.email);
 
-        if (user?.password !== data.password) {
-            throw new NotFoundException(`No se encontró el usuario '${data.email}'`);
+        if (!user) {
+            throw new UnauthorizedException('Email is wrong');
+        }
+
+        const isValidPassword = await bcryptjs.compare(data.password, user.password);
+
+        if (!isValidPassword) {
+            throw new UnauthorizedException('Password is wrong');
         }
 
         const payload = { sub: user.id, username: user.email, roles: user.role };
